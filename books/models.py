ee7287db3045f11e62b9f1f1873980656444d415
bolonge <qhsghd5357@gmail.com>
2022-03-15 22:58:47 +0900

    add model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+from core.models import BaseModel
+
+
+class BooksModel(BaseModel):
+    class Meta:
+        db_table = "books"
+
+    user = models.ForeignKey(
+        'users.UserModel', related_name="books", on_delete=models.CASCADE)
+    store = models.ForeignKey(
+        'bookstore.BookStoreModel', related_name="books", on_delete=models.CASCADE)
+    book_name = models.CharField(max_length=255)
+    rating = models.IntegerField(default=0)
+    book_img = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    book_views = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.book_name
+
+
+class BookRentByUser(BaseModel):
+    user_rented = models.ForeignKey(
+        "users.UserModel", related_name="rent", on_delete=models.CASCADE)
+    book = models.ForeignKey(
+        "BooksModel", related_name="rent", on_delete=models.CASCADE)
+    is_rent = models.BooleanField(default=False)
