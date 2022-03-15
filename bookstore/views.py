7aa143c7556717de64db04bc938baff8c45057a1
kbh224 <one0357@gmail.com>
2022-03-15 21:56:06 +0900

    메인페이지&디테일 페이지 레이아웃

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from bookstore.models import BookStoreModel
+from users.models import UserModel
+
 # Create your views here.
+
+def main(request):
+    stores = BookStoreModel.objects.all()
+    return render(request, 'bookstore/main.html', {'stores':stores})
+
+def detail(request, user_pk):
+    owner = UserModel.objects.get(pk=user_pk)
+    store = BookStoreModel.objects.get(pk=user_pk)
+    return render(request, 'bookstore/store.html', {'owner':owner, 'store':store})
