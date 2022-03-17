8d3a87040a3bbf3dfb1b62aa573bd565e40ec34f
kbh224 <one0357@gmail.com>
2022-03-17 21:26:49 +0900

    main&store page updated

---
+++ static/js/store.js
@@ -0,0 +1,17 @@
+// 모달팝업창
+$(function(){
+    $(".upload").click(function(){
+        $("#upload_books").show();
+        console.log('hello');
+    })
+})
+$(function(){
+
+        // .modal안에 button을 클릭하면 .modal닫기
+        $(".modal button").click(function(){
+            $("#upload_books").hide();
+            location.reload();
+            console.log('refresh')
+        });
+    });
+
