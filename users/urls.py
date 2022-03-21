79903c924a13234d5d12c7883993ae7316b353bf
bolonge <qhsghd5357@gmail.com>
2022-03-21 11:53:08 +0900

    user profile update

--- users/urls.py
+++ users/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('sign-up/', views.sign_up_view, name='sign-up'),
     path('sign-in/', views.sign_in_view, name='sign-in'),
     path('logout/', views.logout, name='logout'),
+    path('my-page/', views.my_profile_page, name='my-page'),
+    path('api/upload-user-img', views.update_image, name='upload-user-img'),
 ]
