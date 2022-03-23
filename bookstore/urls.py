57e4a3d9158cb103799d66cc0e772a91613f1c83
bolonge <qhsghd5357@gmail.com>
2022-03-23 10:25:16 +0900

    fix url

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('mystore/<int:pk>', views.mystore, name='mystore'),
-    path('/<int:store_pk>', views.store, name='store'),
+    path('<int:store_pk>', views.store, name='store'),
     path('store/detail/<int:book_pk>', views.detail, name='detail'),
 ]
