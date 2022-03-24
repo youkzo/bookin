dc884c00b628ce036cbeb27e55f745e058d49d7e
HANJANGWON <wkddnjsdl00@naver.com>
2022-03-24 11:49:00 +0900

    set like model

--- like/models.py
+++ like/models.py
@@ -1,1 +1,14 @@
+from django.db import models
 
+from core.models import BaseModel
+
+
+class LikeModel(BaseModel):
+    class Meta:
+        db_table = "like"
+
+    user_id = models.ForeignKey(
+        'users.UserModel', related_name="like", on_delete=models.CASCADE)
+
+    book_id = models.ForeignKey(
+        'books.BooksModel', related_name="like", on_delete=models.CASCADE)
