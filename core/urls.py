5ee3196527f8cc851b59f5b260ab8593ef2d6cae
bolonge <qhsghd5357@gmail.com>
2022-04-04 22:57:26 +0900

    url valid

--- core/urls.py
+++ core/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('.well-known/pki-validation/<str:filename>',
-         views.validation, name='pki-validation'),
+         views.pki_validation, name='pki-validation'),
     path('.well-known/<str:filename>', views.validation, name='validation'),
 ]
--- core/views.py
+++ core/views.py
@@ -13,3 +13,14 @@ def validation(request, filename):
         url = "https://s3-%s.amazonaws.com/%s/static/%s" % (
             location, bucket, filename)
         return redirect(url)
+
+
+def pki_validation(request, filename):
+    if request.method == 'GET':
+        bucket = settings.AWS_S3_BUCKET_NAME
+
+        location = CoreConfig.s3.get_bucket_location(
+            Bucket=bucket)['LocationConstraint']
+        url = "https://s3-%s.amazonaws.com/%s/static/%s" % (
+            location, bucket, filename)
+        return redirect(url)
