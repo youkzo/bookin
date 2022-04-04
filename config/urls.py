f1940bf44bce59b7ebdd121242a6f1f247b1d946
bolonge <qhsghd5357@gmail.com>
2022-04-04 23:16:56 +0900

    del url

--- config/urls.py
+++ config/urls.py
@@ -23,6 +23,5 @@ urlpatterns = [
     path('', include('books.urls')),
     path('', include('chat.urls')),
     path('', include('search.urls')),
-    path('', include('core.urls')),
     path('bookstore/', include('bookstore.urls')),
 ]
--- core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('.well-known/pki-validation/<str:filename>',
-         views.pki_validation, name='pki-validation'),
-    path('.well-known/<str:filename>', views.validation, name='validation'),
-]
