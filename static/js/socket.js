817d674dd1f62ccac998d4ba7b91e8eac92c59ae
bolonge <qhsghd5357@gmail.com>
2022-04-05 18:43:17 +0900

    https

--- static/js/socket.js
+++ static/js/socket.js
@@ -1,4 +1,4 @@
-const socket = io("http://chat.bookin.link:8000");
+const socket = io("https://chat.bookin.link:8000");
 const input = document.getElementById("input");
 const messageBox = document.getElementById("messageBox");
 
