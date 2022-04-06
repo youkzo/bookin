cd184a1137d074c9cae8198213fb1d8b4e8989b7
bolonge <qhsghd5357@gmail.com>
2022-04-06 19:44:23 +0900

    delete import

--- books/views.py
+++ books/views.py
@@ -1,9 +1,6 @@
-from asyncio.windows_events import NULL
-import datetime
 from django.shortcuts import redirect, render
-from books.models import BookRentByUser, BookReview, BooksModel
-from django.utils.datastructures import MultiValueDictKeyError
 
+from books.models import BookRentByUser, BookReview, BooksModel
 from bookstore.models import BookStoreModel
 from chat.models import ChatRoom
 
@@ -11,13 +8,15 @@ from chat.models import ChatRoom
 def main(request):
     stores = BookStoreModel.objects.all()
     return render(request, 'bookstore/main.html', {'stores': stores})
-    
-#스토어페이지에서 책 클릭하면 디테일 페이지로 이동
+
+# 스토어페이지에서 책 클릭하면 디테일 페이지로 이동
+
+
 def detail(request, book_pk):
     book = BooksModel.objects.get(pk=book_pk)
     buyer = ChatRoom.objects.filter(participants=request.user.pk)
-    
-    #해당책후기 가져오기
+
+    # 해당책후기 가져오기
     reviews = BookReview.objects.filter(book_id=book_pk)
 
     # 도서정보수정
@@ -25,10 +24,10 @@ def detail(request, book_pk):
 
         # try:
         #     if request.POST['is_rent'] == 'on':
-        #         status = True   
+        #         status = True
         # except MultiValueDictKeyError:
         #     status = False
-        
+
         # 도서대여상태 체크박스 체크/해제 여부
         is_rent = len(request.POST.getlist('is_rent'))
         print(is_rent)
@@ -50,21 +49,19 @@ def detail(request, book_pk):
             rented_info = BookRentByUser.objects.create(
                 # updated_at = datetime.date.today(),
                 # created_at = datetime.date.today(),
-                book_id = book.pk,
-                user_rented_id = request.POST['user_rented_id'],
-                exp_date = request.POST['returnday'],
+                book_id=book.pk,
+                user_rented_id=request.POST['user_rented_id'],
+                exp_date=request.POST['returnday'],
             )
             return redirect('detail', book.pk)
 
         except ValueError:
             return redirect('detail', book.pk)
 
-            
-
     context = {
-        'book':book, 
-        'buyer':buyer, 
-        'reviews':reviews
+        'book': book,
+        'buyer': buyer,
+        'reviews': reviews
     }
 
     return render(request, 'bookstore/detail.html', context)
@@ -73,14 +70,14 @@ def detail(request, book_pk):
 def review(request, book_pk):
     book = BooksModel.objects.get(pk=book_pk)
 
-    #리뷰작성
+    # 리뷰작성
     if request.method == "POST":
         review = BookReview.objects.create(
-            content = request.POST['content'],
-            review_rating = request.POST['review_rating'],
-            writer_id = request.user.pk,
-            book_id = book_pk
+            content=request.POST['content'],
+            review_rating=request.POST['review_rating'],
+            writer_id=request.user.pk,
+            book_id=book_pk
         )
         return redirect('my-page')
 
-    return render(request, 'bookstore/review.html', {'book':book})
+    return render(request, 'bookstore/review.html', {'book': book})
