9899b81af57b9b69ae10bc51fe19ef8b6affb6c7
bolonge <qhsghd5357@gmail.com>
2022-04-08 13:55:46 +0900

    mypage update

--- users/models.py
+++ users/models.py
@@ -7,7 +7,8 @@ class UserModel(AbstractUser):
         db_table = "my_user"
     username = models.CharField(max_length=30, unique=True)
     email = models.EmailField(unique=True)
-    user_image = models.CharField(max_length=255, blank=True, null=True)
+    user_image = models.CharField(
+        max_length=255, default="https://bookin-bucket.s3.ap-northeast-2.amazonaws.com/static/bookin_default.jpg")
     email_code = models.CharField(max_length=50, blank=True, null=True)
     location = models.CharField(max_length=50, blank=True, null=True)
 
