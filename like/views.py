936ebcd6f6df3e72013d8be7dd4c3004c1152e2e
HANJANGWON <wkddnjsdl00@naver.com>
2022-04-09 18:21:00 +0900

    update like

--- like/views.py
+++ like/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import get_object_or_404, render
+from like.services.like_service import do_like, undo_like
+from books.models import BooksModel
 from like.models import LikeModel
+
+
+def like_book(request):
+    user = request.user
+    book = request.POST.get('book_id')
+    like = LikeModel.objects.filter(user=user.id, book=book).exists()
+
+    if like:
+        undo_like(user.id, book)
+        print('싫어요')
+    else:
+        do_like(user.id, book)
+        print('좋아요')
+    return JsonResponse({"status": 'Success'})
