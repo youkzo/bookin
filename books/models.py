72abe4c2c4cf392cd786a7817fea62ac209cc3c6
bolonge <qhsghd5357@gmail.com>
2022-04-10 19:41:09 +0900

    book default img

--- books/models.py
+++ books/models.py
@@ -16,7 +16,8 @@ class BooksModel(BaseModel):
     price = models.IntegerField(default=0)
     content = models.TextField()
     rating = models.IntegerField(default=0)
-    book_img = models.CharField(max_length=255)
+    book_img = models.CharField(
+        max_length=255, default='https://bookin-bucket.s3.ap-northeast-2.amazonaws.com/static/bookin_default.jpg')
     category = models.CharField(max_length=255)
     book_views = models.IntegerField(default=0)
     is_rented = models.BooleanField(default=False)
@@ -34,11 +35,11 @@ class BookRentByUser(BaseModel):
         "BooksModel", related_name="rent", on_delete=models.CASCADE)
     exp_date = models.DateField(null=True)
 
+
 class BookReview(BaseModel):
     content = models.TextField()
-    review_rating =  models.IntegerField(default=0)
+    review_rating = models.IntegerField(default=0)
     writer = models.ForeignKey(
-        "users.UserModel", related_name="review", on_delete=models.CASCADE)   
+        "users.UserModel", related_name="review", on_delete=models.CASCADE)
     book = models.ForeignKey(
         "BooksModel", related_name="review", on_delete=models.CASCADE)
-        
