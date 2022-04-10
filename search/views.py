b76652cbe9e92bf7ad7febc271fffbba89a303ec
bolonge <qhsghd5357@gmail.com>
2022-04-10 19:34:46 +0900

    search update

--- search/views.py
+++ search/views.py
@@ -5,10 +5,6 @@ from search.services import search_book_and_bookstore
 
 def search_all(request):
     if request.method == 'GET':
-        user = request.user.is_authenticated
-        if user:
-            search_word = request.GET.get('word')
-            books, bookstores = search_book_and_bookstore(search_word)
-            return render(request, 'search/search_main.html', {'books': books, 'bookstores': bookstores, 'word': search_word})
-        else:
-            return redirect('/')
+        search_word = request.GET.get('word')
+        books, bookstores = search_book_and_bookstore(search_word)
+        return render(request, 'search/search_main.html', {'books': books, 'bookstores': bookstores, 'word': search_word})
